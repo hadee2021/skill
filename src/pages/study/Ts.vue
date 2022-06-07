f890166e9b3441c58ffe65c02df34fce397208d3
hadee2021 <tpghks9920@naver.com>
2022-06-07 19:45:50 +0900

    220607

--- src/pages/study/Ts.vue
+++ src/pages/study/Ts.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    Ts
+    <Content
+    :jsonArr="TsArr"
+    @toggle-modal="openModal"
+    />
+    <teleport to='#modal'>
+      <Modal 
+        v-if="showModal"
+        @close="closeModal"
+        :imgData="imgData"
+      />
+    </teleport>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import TSJSON from '@/Json/Ts.json'
+import Content from '@/components/Content.vue'
+import Modal from '@/components/Modal.vue'
 export default {
+  components: {
+    Content,
+    Modal
+  },
+  setup() {
+    const TsArr = ref(TSJSON)
+    const showModal = ref(false)
+    const imgData = ref('')
 
+    const openModal = (data) => {
+      console.log(data)
+      showModal.value = true
+      imgData.value = data
+    }
+
+    const closeModal = () => {
+      showModal.value = false
+      imgData.value = ''
+    }
+
+    return {
+      TsArr,
+      showModal,
+      openModal,
+      closeModal,
+      imgData,
+    }
+  }
 }
 </script>
 
--- src/pages/study/TsNav.vue
+++ src/pages/study/TsNav.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    
+    <Navigation
+      :jsonArr="TsArr"
+    />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import TSJSON from '@/Json/Ts.json'
+import Navigation from '@/components/Navigation.vue'
 export default {
-
+  components: {
+    Navigation
+  },
+  setup() {
+    const TsArr = ref(TSJSON)
+    return {
+      TsArr
+    }
+  }
 }
 </script>
 

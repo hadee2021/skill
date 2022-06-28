2b6f0956ac88f854b0f28bd00e482a722ddb61dd
hadee2021 <tpghks9920@naver.com>
2022-06-28 19:55:14 +0900

    0628

--- src/pages/study/React.vue
+++ src/pages/study/React.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    React
+    <Content
+    :jsonArr="ReactArr"
+    @toggle-modal="openModal"
+    />
+    <teleport to='#modal'>
+      <Modal 
+        v-if="showModal"
+        @close="closeModal"
+        :imgData="imgData"
+      />
+    </teleport>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import REACTJSON from '@/Json/React.json'
+import Content from '@/components/Content.vue'
+import Modal from '@/components/Modal.vue'
 export default {
+  components: {
+    Content,
+    Modal
+  },
+  setup() {
+    const ReactArr = ref(REACTJSON)
+    const showModal = ref(false)
+    const imgData = ref('')
 
+    const openModal = (data) => {
+      console.log(data)
+      showModal.value = true
+      imgData.value = data
+    }
+
+    const closeModal = () => {
+      showModal.value = false
+      imgData.value = ''
+    }
+
+    return {
+      ReactArr,
+      showModal,
+      openModal,
+      closeModal,
+      imgData,
+    }
+  }
 }
 </script>
 
--- src/pages/study/ReactNav.vue
+++ src/pages/study/ReactNav.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    
+    <Navigation
+      :jsonArr="ReactArr"
+    />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import REACTJSON from '@/Json/React.json'
+import Navigation from '@/components/Navigation.vue'
 export default {
-
+  components: {
+    Navigation
+  },
+  setup() {
+    const ReactArr = ref(REACTJSON)
+    return {
+      ReactArr
+    }
+  }
 }
 </script>
 
